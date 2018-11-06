f2b9b17bcd18c90886974f274cfd825d6db43cc8
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-06 02:34:43 +0000

    updated with LazyHTML

--- src/index.js
+++ src/index.js
@@ -1,59 +1,49 @@
+/* global ASSET_PATH */
 /* global fetch */
 import React, { Component, Fragment } from 'react'
 import { Link, NavLink, Route, Switch } from 'react-router-dom'
+import LazyHTML from 'components/lazy-html'
 import SiteBanner from 'components/site-banner'
 
-class PolicyGuidePage extends Component {
+const baseurl = ASSET_PATH + "assets/plugins/fscp-react-component/"
 
-  constructor(props) {
-    super(props)
-    this.mounted = false
-    this.state = {}
-  }
+const pages = [
+  { display: 'Introduction', route: 'introduction', filename: '0-introduction' },
+  { display: '1. Objectives', route: 'objectives', filename: '1-objectives' },
+  { display: '2. Scope & Applicability', route: 'scope', filename: '2-scope' },
+  { display: '3. Three-Step Software Solutions Analysis', route: 'solutions-analysis', filename: '3-solutions-analysis' },
+  { display: '4. Government-Wide Code Reuse', route: 'code-reuse', filename: '4-code-reuse' },
+  { display: '5. Open Source Software', route: 'open-source', filename: '5-open-source' },
+  { display: '6. Exceptions to Government Code Reuse', route: 'exceptions', filename: '6-exceptions' },
+  { display: '7. Implementation', route: 'implementation', filename: '7-implementation' },
+  { display: 'Appendix A - Definitions', route: 'appendix', filename: 'appendix' }
+]
 
-  componentDidMount() {
-    this.mounted = true
-    if (this.props.url) {
-      fetch(this.props.url)
-      .then(response => response.text())
-      .then(html => {
-        if (!this.state.html && this.mounted) {
-          this.setState({ html })
-        }
-      })
-    }
-  }
-
-  componentWillUnmount() {
-    this.mounted = false
-  }
-
-  render() {
-    if (this.state.html) {
-      return <div dangerouslySetInnerHTML={{ __html: this.state.html }} style={{background: 'white', padding: '2em 2em 3em', wordBreak: 'break-word'}}></div>
-    } else {
-      return <div></div>
-    }
-  }
+const PolicyGuidePage = ({ url }) => {
+ return <div className="docs-content"><LazyHTML url={url}/></div>
 }
 
 export default class PolicyGuide extends Component {
 
+  get routes() {
+    return pages.map(({ route, filename }) => {
+      const path = `${this.props.match.url}/${route}`
+      const url = baseurl + filename + '.html'
+      return <Route key={path} path={path} component={() => <PolicyGuidePage url={url}/>} />
+    })
+  }
+
+  get sidenav() {
+    return pages.map(({ display, route }) => (
+      <li key={route}>
+        <NavLink activeClassName="current" to={`${this.props.match.url}/${route}`}>{display}</NavLink>
+      </li>
+    ))
+  }
+
   render() {
     console.log("this.props.match:", this.props.match)
-    const baseurl = "https://raw.githubusercontent.com/GSA/code-gov-policy-guide/master/pages/html/"
     const matchUrl = this.props.match.url
-    const pages = [
-      { display: 'Introduction', route: 'introduction', filename: '0-introduction' },
-      { display: '1. Objectives', route: 'objectives', filename: '1-objectives' },
-      { display: '2. Scope & Applicability', route: 'scope', filename: '2-scope' },
-      { display: '3. Three-Step Software Solutions Analysis', route: 'solutions-analysis', filename: '3-solutions-analysis' },
-      { display: '4. Government-Wide Code Reuse', route: 'code-reuse', filename: '4-code-reuse' },
-      { display: '5. Open Source Software', route: 'open-source', filename: '5-open-source' },
-      { display: '6. Exceptions to Government Code Reuse', route: 'exceptions', filename: '6-exceptions' },
-      { display: '7. Implementation', route: 'implementation', filename: '7-implementation' },
-      { display: 'Appendix A - Definitions', route: 'appendix', filename: 'appendix' }
-    ]
     return (
       <div>
         <SiteBanner title='Federal Source Code Policy' />
@@ -68,20 +58,14 @@ export default class PolicyGuide extends Component {
           <div className="width-quarter">
             <nav className="sidebar left">
               <ul>
-              {pages.map(({ display, route }) => {
-                return <li key={route}><NavLink activeClassName="current" to={`${matchUrl}/${route}`}>{display}</NavLink></li>
-              })}
+                {this.sidenav}
               </ul>
             </nav>
           </div>
-          <div className="width-three-quarters">
+          <Switch>
             <Route exact path="/" render={() => <Redirect to={`${this.props.match.url}/introduction`} /> } />
-            {pages.map(({ route, filename }) => {
-              const path = `${matchUrl}/${route}`
-              const url = baseurl + filename + '.html'
-              return <Route key={path} path={path} component={() => <PolicyGuidePage url={url}/>} />
-            })}
-          </div>
+            {this.routes}
+          </Switch>
         </div>
       </div>
     )
